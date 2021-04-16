2a2012d0b59d25764cf4cfe3a0a330a94d7566f8
SzarneckiP <siarniuk@gmail.com>
2021-04-16 17:32:45 +0200

    add layauts

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-const { response } = require('express');
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+const { extname } = require('path');
 
 const app = express();
+app.engine('hbs', hbs({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-        res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
-
-app.use('/user', (req, res, next) => {
-    res.show('forbidden.html');
-    next();
-});
-
+app.use(express.static(path.join(__dirname, '/views/layouts')));
 app.use(express.static(path.join(__dirname, '/views')));
 
 app.get('/', (req, res) => {
-    res.show('home.html');
+    res.render('home');
 });
 
 app.get('/home', (req, res) => {
-    res.show('home.html');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about', { layout: "dark" });
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
+});
+
+app.use('/user/', (req, res) => {
+    res.render('forbidden');
 });
 
 app.use((req, res) => {
-    res.status(404).show('404.html');
+    res.status(404).render('404');
 });
 
 app.listen(8000, () => {
