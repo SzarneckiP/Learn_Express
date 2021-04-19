9258360d9e17d6eb1c8737435b0b5fcdc48bd691
SzarneckiP <siarniuk@gmail.com>
2021-04-19 09:36:30 +0200

    Add send-contact

--- server.js
+++ server.js
@@ -1,37 +1,55 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-const { extname } = require('path');
 
 const app = express();
 app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/views/layouts')));
-app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, '/views/*')));
+app.use(express.static(path.join(__dirname, '/views/404')));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/contact/send-message', (req, res) => {
+
+    const { author, sender, title, message, file } = req.body;
+
+    if (author && sender && title && message && file) {
+        res.render('contact/contact', { isSent: true, fileName: file });
+    }
+    else {
+        res.render('contact/contact', { isError: true });
+    }
+});
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home/home');
 });
 
 app.get('/home', (req, res) => {
-    res.render('home');
+    res.render('home/home');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { layout: "dark" });
+    res.render('about/about', { layout: "dark" });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact/contact');
 });
 
 app.get('/hello/:name', (req, res) => {
-    res.render('hello', { name: req.params.name });
+    res.render('hello/hello', { name: req.params.name });
 });
 
 app.use('/user/', (req, res) => {
-    res.render('forbidden');
+    res.render('forbidden/forbidden');
 });
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404/404');
 });
 
 app.listen(8000, () => {
